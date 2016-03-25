abeaf4c576cf8bd391982cd897a61c01cdc4e5d4
rickie120243 <rickie120243@gmail.com>
2016-03-25 13:51:16 +0800

    fix style

--- sortsyllable.js
+++ sortsyllable.js
@@ -1,4 +1,4 @@
-function sortSyllable (content) {
+function sortSyllable(content) {
   if ('string' !== typeof content) {
     return [];
   }
@@ -6,7 +6,7 @@ function sortSyllable (content) {
   return doSort(tokens);
 }
 
-function tokenize (str) {
+function tokenize(str) {
   var obj = {}
   str.replace(/[\u0f00-\u0f0a\u0f10-\u0fff]+/g, function(syl) {
     obj[syl] = ++obj[syl] || 1;
